2bb71670f1ea9792c4341221250fe7627c48fedc
rovestrada <rovestrada@gmail.com>
2022-09-07 17:40:00 -0500

    Codigo Base

---
+++ Electraton/Codigos Electraton Master MAR - SEP 2022/Electraton_Temps_Volt_DRS_V1.4/Electraton_Temps_Volt_DRS_V1.4.ino
@@ -0,0 +1,184 @@
+//Essential libraries for OLED and Servomotor
+//Se remueve funcionalidad de drs
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Adafruit_MLX90614.h> //Melexis
+#include "patros.h"
+
+//Declaracion de MLX
+Adafruit_MLX90614 termoDI = Adafruit_MLX90614(); // DI = Delantero Izquierdo
+Adafruit_MLX90614 termoDD = Adafruit_MLX90614(); // DD = Delantero Derecho
+Adafruit_MLX90614 termoTI = Adafruit_MLX90614(); // TI = Trasero Izquierdo
+Adafruit_MLX90614 termoTD = Adafruit_MLX90614(); // TD = Trasero Derecho
+
+//i2c address space
+//U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+//Variables to use
+int b_DRS = 9;                                        //Button DRS
+int p_DRS = 10;                                        //Braking pedal
+int pin_DRS=6;                                        //PWM drs
+bool state_DRS = false;                               //State of DRS active or not
+int BV1 = A0, BV2 = A1, BV3 = A2, BV4 = A3;           //Battery, analog a0-a3
+int B4State, B3State, B2State, B1State = 0;           //Button states
+double B1Value, B2Value, B3Value, B4Value = 0;        //Batterry Values 1-4
+double BATVAL = 0;                                    //Battery Values
+float temp1 = 0, temp2 = 58.51, temp3 = 365.1, temp4 = 12.521; //Temparature values
+int bShowMode = 12;                                   //Boton de showmode
+bool showState = false;                               //Show State mode
+int firstpress = 0;                                   //Count of first press
+int newtime = 0;                                      //New time
+float tempDI = 0.0, tempDD = 0.0, tempTI = 0.0, tempTD = 0.0;
+
+
+
+void setup() {
+  Serial.begin(57600);
+  
+  pinMode(b_DRS, INPUT);
+  pinMode(p_DRS, INPUT);
+  pinMode(pin_DRS, OUTPUT);
+  pinMode(bShowMode, INPUT);
+  termoDI.begin(0x5A);//Melexi Delantero Izquierdo 0x5A
+  termoDD.begin(0x5B);//Melexi Delantero Derecho 0x5B
+  termoTI.begin(0x5C);//Melexi Trasero Izquierdo 0x5C
+  termoTD.begin(0x5D);//Melexi Trasero Derecho 0x5D
+  u8g2.setBusClock(100000);
+  u8g2.begin();
+  delay(200);
+}
+
+void loop() {
+  if (digitalRead(bShowMode) == 0) {
+    firstpress = millis();
+    while (digitalRead(bShowMode) == 0) {
+      newtime = millis();
+    }
+    if (newtime - firstpress >= 3000) {
+      showState = true;
+    }
+  }
+  if (showState == false) {
+    //VB
+    VB();
+    //Temperatura
+    temp();
+    //    //DRS functionality
+    if (digitalRead(b_DRS) == LOW && state_DRS == false) {
+      while (digitalRead(b_DRS) == LOW) {
+//        DRS_ON();
+      }
+
+    }
+    else if ((digitalRead(b_DRS) == LOW || digitalRead(p_DRS) == HIGH) && state_DRS == true) {
+      while (digitalRead(b_DRS) == LOW || digitalRead(p_DRS) == HIGH) {
+        DRS_OFF();
+      }
+    }
+    infodisplay();
+  }
+
+  else if (showState == true) {
+    showMode();
+  }
+
+}
+
+void VB() {
+  B1Value = analogRead(A0);
+  B2Value = analogRead(A1);
+  B3Value = analogRead(A2);
+  B4Value = analogRead(A3);
+  BATVAL = ((B1Value + B2Value + B3Value + B4Value) / 4) / 1001.2 * 100;
+  B1State = map(B1Value, 0, 1002, 0, 5);
+  B2State = map(B2Value, 0, 1002, 0, 5);
+  B3State = map(B3Value, 0, 1002, 0, 5);
+  B4State = map(B4Value, 0, 1002, 0, 5);
+  delay(10);
+}
+
+void temp() {
+  tempDI = termoDI.readObjectTempC(); //0x5A
+  Serial.println(tempDI);
+  tempDD = termoDD.readObjectTempC(); //0x5B
+  Serial.println(tempDD);
+  tempTI = termoTI.readObjectTempC(); //0x5C
+  Serial.println(tempTI);
+  tempTD = termoTD.readObjectTempC(); //0x5D
+  Serial.println(tempTD);
+
+}
+
+void DRS_ON() {
+  //delay(100);
+  state_DRS = true;
+  analogWrite(pin_DRS,255);
+  VB();
+  temp();
+  infodisplay();
+}
+
+void DRS_OFF() {
+  //delay(100);
+  state_DRS = false;
+  analogWrite(pin_DRS,255);
+  VB();
+  temp();
+  infodisplay();
+}
+
+void infodisplay() {
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_crox2cb_tn);
+    u8g2.setCursor(5, 26);
+    u8g2.print(temp1, 1);
+    u8g2.setCursor(5, 43);
+    u8g2.print(temp2, 1);
+    u8g2.setCursor(70, 26);
+    u8g2.print(temp3, 1);
+    u8g2.setCursor(70, 43);
+    u8g2.print(temp4, 1);
+
+    if (state_DRS == true) {
+      u8g2.setFont(u8g2_font_squeezed_b7_tr);
+      u8g2.drawFrame(108, -1, 30, 12);
+      u8g2.drawStr(111, 9, "DRS");
+    }
+    u8g2.setFont(u8g2_font_crox1hb_tf);
+    u8g2.drawHLine(0, 51, 128);
+    u8g2.setCursor(1, 63);
+    u8g2.print(BATVAL, 1);
+    u8g2.drawStr(27, 63, "%");
+    u8g2.setFont(u8g2_font_battery19_tn);
+    u8g2.setFontDirection(1);
+    u8g2.setCursor(40, 54);
+    u8g2.print(B1State);
+    u8g2.setCursor(62, 54);
+    u8g2.print(B2State);
+    u8g2.setCursor(84, 54);
+    u8g2.print(B3State);
+    u8g2.setCursor(106, 54);
+    u8g2.print(B4State);
+    u8g2.setFontDirection(0);
+  } while (u8g2.nextPage());
+}
+
+void showMode() {
+
+  u8g2.firstPage();
+  do {
+    u8g2.drawXBMP(2, 0, troquemsa_width, troquemsa_height, troquemsa_bits);
+  } while (u8g2.nextPage());
+  delay(3000);
+
+  u8g2.firstPage();
+  do {
+    u8g2.drawXBMP(12, 0, moldem_width, moldem_height, moldem_bits);
+  } while (u8g2.nextPage());
+  delay(3000);
+}
---
+++ Electraton/Codigos Electraton Master MAR - SEP 2022/Electraton_Velocim_OT_V1.4/Electraton_Velocim_OT_V1.4.ino
@@ -0,0 +1,174 @@
+//Librerias
+#include <SPI.h>
+#include <Wire.h>
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <FreqCount.h>
+#include "carro.h"
+
+//Characteristics of OLED
+U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+//Variables to use
+float diameter = 0.24;                            //Wheel diameter in meters
+float ms = 0;                                     //Velocity m/s
+float velocidad = 0;                              //Velocity kmh
+int b_OT = 9;                                     //OT button pin
+float bState_OT = 0;                              //OT button state
+float timer_OT = 0;                               //OT on timer
+bool state_OT = false;                            //OT state
+int availability_OT = 0;                          //OT availability
+float countdown = 0;                              //OT time remaining
+int LM3914 = 0;                                   //PWM value for LEDs
+int relay = 8;                                    //Relay pin
+int leds = 6;                                     //LEDs pin
+int bShowMode = 12;                               //Button showmode
+int brake = 10;                                   //Brake input
+bool showState = false;                               //Show State mode
+int firstpress = 0;                                   //Count of first press
+int newtime = 0;                                      //New time
+//
+int radio = 56;
+int altura = 92;
+int centrox = 64;
+float angulo = 0;
+int posx = 0;
+int posy = 0;
+float angesq = 0;
+int orig1x = 0, orig1y = 0, orig2x = 0, orig2y = 0;
+//
+
+void setup() {
+  Serial.begin(57600);
+  pinMode(bShowMode, INPUT);
+  pinMode(brake, INPUT);
+  pinMode(b_OT, INPUT);
+  pinMode(relay, OUTPUT);
+  pinMode(leds, OUTPUT);                                  //wait for the OLED to power up
+  u8g2.begin();
+  digitalWrite(relay, !state_OT);                 //Relay starts off
+  FreqCount.begin(1000);
+}
+
+void loop() {
+  if (digitalRead(bShowMode) == 0) {
+    firstpress = millis();
+    Serial.print(firstpress);
+    while(digitalRead(bShowMode) == 0) {
+      newtime = millis();
+      Serial.println(newtime - firstpress);
+    }
+    if (newtime - firstpress >= 3000) {
+      showState = true;
+    }
+  }
+  if (showState == false) {
+    digitalWrite(relay, !state_OT);                 //Toggle relay por seguridad
+    if (FreqCount.available()) {
+      velocity();
+    }
+    //Sección botones
+        if (digitalRead(brake == 1)) {
+          if (state_OT == true) {
+            digitalWrite(leds, 0);
+            state_OT = false;
+          }
+        }
+    else if (digitalRead(b_OT) == 0 && state_OT == false) {
+      while (digitalRead(b_OT) == 0) {
+      }       //No empieza hasta que se libera y no se alterna
+      state_OT = true;
+      bState_OT = millis();
+    }
+    else if (digitalRead(b_OT) == 0 && state_OT == true) {
+      while (digitalRead(b_OT) == 0) {
+      }       //No empieza hasta que se libera y no se alterna
+      digitalWrite(leds, 0);
+      state_OT = false;
+    }
+    //Sección OT
+    if (state_OT == true) {
+      func_OT();
+    }
+    infodisplay();
+  }
+  else if (showState == true) {
+    showMode();
+  }
+}
+
+void velocity() {
+  unsigned long count = (FreqCount.read());
+  ms = (PI * diameter * count);
+  velocidad = ms * 3.6;
+  angulo = ((velocidad / 60) * 1.047 - 1.047);
+  angesq = (angulo / 1.047) * PI / 2;
+  posx = (radio - 4) * sin(angulo) + 64;
+  posy = -(radio - 4) * cos(angulo) + 92;
+  orig1x = 3 * cos(angesq - PI) + 64;
+  orig1y = 3 * sin(angesq - PI) + 60;
+  orig2x = 3 * cos(angesq) + 64;
+  orig2y = 3 * sin(angesq) + 60;
+}
+
+void func_OT() {
+  countdown = ((30000 - timer_OT) / 1000);
+  LM3914 = ((countdown + 5) * 3.1);
+  analogWrite(leds, (LM3914));
+  timer_OT = (millis()) - (bState_OT);
+  if (timer_OT >= 30000) {
+    state_OT = false;
+    analogWrite(leds, 0);
+  }
+}
+
+void infodisplay() {
+  u8g2.firstPage();
+  do {
+    //u8g2.setFont(u8g2_font_luRS24_tr);
+    u8g2.setFont(u8g2_font_logisoso32_tn);
+    u8g2.setCursor(4, 34);
+    u8g2.print(velocidad, 2);
+    u8g2.drawCircle(centrox, altura, radio, U8G2_DRAW_UPPER_LEFT | U8G2_DRAW_UPPER_RIGHT);
+    u8g2.drawCircle(centrox, 92, 54, U8G2_DRAW_UPPER_LEFT | U8G2_DRAW_UPPER_RIGHT);
+    //u8g2.drawHLine(60,63,4);
+    u8g2.drawLine(orig1x, orig1y, posx, posy);
+    u8g2.drawLine(orig2x, orig2y, posx, posy);
+    //u8g2.drawTriangle(,,3*cos(angesq)+64,3*sin(angesq)+60,posx,posy);
+    u8g2.drawDisc(64, 64, 7, U8G2_DRAW_UPPER_LEFT | U8G2_DRAW_UPPER_RIGHT);
+
+    if (state_OT == true) {
+      u8g2.setFont(u8g2_font_t0_11_me);
+      u8g2.setCursor(4, 50);
+      u8g2.print("OT:");
+      u8g2.setCursor(4, 60);
+      u8g2.print(countdown, 0);
+    }
+
+  } while (u8g2.nextPage());
+}
+
+void showMode() {
+  for (int pos = -70; pos < 160; pos++) {
+    u8g2.firstPage();
+    u8g2.setBitmapMode(1);
+    do {
+      if (pos == 21) {
+        u8g2.drawXBMP(22, 0, shield_width, shield_height, shield_bits);
+        u8g2.drawXBMP(pos, 0, carStop_width, carStop_height, carStop_bits);
+      }
+      else {
+        u8g2.drawXBMP(22, 0, shield_width, shield_height, shield_bits);
+        if (pos % 2) {
+          u8g2.drawXBMP(pos, 0, car1_width, car1_height, car1_bits);
+        }
+        else {
+          u8g2.drawXBMP(pos, 0, car2_width, car2_height, car2_bits);
+        }
+      }
+    } while (u8g2.nextPage());
+    if (pos == 21) {
+      delay(3000);
+    }
+  }
+}
